003f83571fc3c95180a061cbc42f290002be9347
Samuel <samuel.jesus@martinbrower.com.br>
2019-11-22 16:13:45 -0300

    Carregamento ao procurar usuário e salvar no storage

--- modulo01/.eslintrc.js
+++ modulo01/.eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/state-in-constructor':'off',
     'react/static-property-placement': 'off',
-    'react/jsx-props-no-spreading': 'off'
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off'
   },
 };
--- modulo01/src/pages/Main/index.js
+++ modulo01/src/pages/Main/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -17,10 +18,26 @@ import {
   ProfileButtonText,
 } from './styles';
 
-export default function Main() {
+export default function Main({ navigation }) {
   const [newUser, setNewUser] = useState('');
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  async function fetchUsers() {
+    const data = await AsyncStorage.getItem('users');
+
+    if (data) {
+      setUsers(JSON.parse(data));
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  useEffect(() => {
+    AsyncStorage.setItem('users', JSON.stringify(users));
+  }, [users]);
 
   async function handleAddUser() {
     setLoading(true);
@@ -39,6 +56,10 @@ export default function Main() {
     setLoading(false);
 
     Keyboard.dismiss();
+  }
+
+  function handleNavigate() {
+    navigation.navigate('User');
   }
 
   return (
@@ -71,7 +92,7 @@ export default function Main() {
             <Name>{item.name}</Name>
             <Bio>{item.bio}</Bio>
 
-            <ProfileButton onPress={() => {}}>
+            <ProfileButton onPress={handleNavigate}>
               <ProfileButtonText>Ver perfil</ProfileButtonText>
             </ProfileButton>
           </User>
